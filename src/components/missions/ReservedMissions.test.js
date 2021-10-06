9019d6615cb507ecf1362a3649d8b75dbfdefa86
DanteAlonsoHT <dante1818@hotmail.com>
2021-10-06 18:18:27 -0500

    Create all tests for ReservedMissions component

---
+++ src/components/missions/ReservedMissions.test.js
@@ -0,0 +1,59 @@
+import React from "react";
+import '@testing-library/jest-dom/extend-expect';
+import { create } from "react-test-renderer";
+import { render, screen } from '@testing-library/react';
+import ReservedMissions from "./ReservedMissions";
+import { v4 as uuidv4 } from 'uuid';
+
+const createComponent = create(
+  <ReservedMissions missionName={"Thaicom"} />
+);
+
+const renderComponent = (missionName) => {
+  render(
+    <table className="table table-striped table-hover table-bordered">
+      <tbody>
+        <ReservedMissions key={uuidv4()} missionName={missionName} />
+      </tbody>
+    </table>
+  );
+}
+
+
+describe('Testing ReservedMissions component', () => {
+  test('type for every ReservedMissions is a tr HTML tag', () => {
+    const component = createComponent.toJSON();
+    expect(component.type).toEqual('tr');
+  })
+
+  test('there are no props for ReservedMissions component', () => {
+    const component = createComponent.toJSON();
+    expect(component.props).toEqual({});
+  })
+
+  test('it is a single element with only one child which is td HTML tag', () => {
+    const component = createComponent.toJSON();
+    expect(component.children[0].type).toEqual('td');
+  })
+
+  test('its td HTML tag child has text as a child', () => {
+    const component = createComponent.toJSON();
+    console.log(component);
+    expect(component.children[0].children).toEqual(["Thaicom"]);
+  })
+
+  test('there is not an empty list', () => {
+    renderComponent('Thaicom');
+    expect(screen.getByText('Thaicom')).toBeInTheDocument();
+  })
+
+  test('it can have other names', () => {
+    renderComponent('Telstar');
+    expect(screen.getByText('Telstar')).toBeInTheDocument();
+  })
+
+  test('the td HTML tag (or the cell) contains the mission name', () => {
+    renderComponent('Telstar');
+    expect(screen.getByRole('cell')).toHaveTextContent('Telstar');
+  })
+});
